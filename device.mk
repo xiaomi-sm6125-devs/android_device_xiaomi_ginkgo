b5369c9fe61ff783b14a9ad58c84c592dc9ce671
DarkJoker360 <simoespo159@gmail.com>
2022-01-09 19:06:23 +0100

    ginkgo: Move libnfc-nci to vendor
    Change-Id: Ib10ccd2a3010914b6e61cf2a3da7f2c251d5583f

--- device.mk
+++ device.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
     vendor.nxp.hardware.nfc@2.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
